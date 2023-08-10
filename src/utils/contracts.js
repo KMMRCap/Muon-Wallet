8b63290b9c84387f3a754e28d98c5174af8ed6e9
KMMRCap <mortezarezainia@gmail.com>
2023-08-10 10:58:12 +0330

    clean up : abis cleaned up

--- src/utils/contracts.js
+++ src/utils/contracts.js
@@ -1,6 +1,5 @@
 const Contracts = {
     address: {
-        // MuonFeeUpgradeable: "0xa3d76dd3C0A7550f31FbFb7Ae4dB3826eD7e573E",
         MuonFeeUpgradeable: "0x31249D6c96b4B858a47bece989FBB83E2704A639",
         Token: "0x84102Df4B6Bcb72114532241894B2077428a7f86",
         SmartRouter: "0x9a489505a00cE272eAa5e07Dba6491314CaE3796",
@@ -20,7 +19,7 @@ module.exports = { Contracts }
 
 // MuonFeeUpgradeable :
 // R: users(address)
-// W: deposit(amount) - withdraw(amount)
+// W: deposit(amount)
 
 // Token :
 // R: balanceOf(address) - allowance(account, spender)
