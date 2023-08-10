09c34cc6077004603515a37cce57e80f4aa52d8c
KMMRCap <mortezarezainia@gmail.com>
2023-08-10 12:31:09 +0330

    update : declaration file added

---
+++ src/index.d.ts
@@ -0,0 +1,7 @@
+/**
+ * @param {string} appName The string
+ * @param {string} methodName The string
+ * @param {object} parameters The string
+ * @returns {Promise<any>}
+ */
+export declare function request(appName: string, methodName: string, parameters: object): Promise<any>;
