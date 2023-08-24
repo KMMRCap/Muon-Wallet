c629417bcd566437d0ed44f5871dac9df1f3236a
KMMRCap <mortezarezainia@gmail.com>
2023-08-24 14:48:31 +0330

    update : fee manager contract address changed

--- src/contracts/index.js
+++ src/contracts/index.js
@@ -1,6 +1,6 @@
 const Contracts = {
 	address: {
-		MuonFeeUpgradeable: '0x31249D6c96b4B858a47bece989FBB83E2704A639',
+		MuonFeeUpgradeable: '0x4c00A64Addb992a352e5147b791b310B17432a2b',
 		Token: '0xF43CD517385237fe7A48927073151D12f4eADC53',
 		SmartRouter: '0x9a489505a00cE272eAa5e07Dba6491314CaE3796',
 		Pair: '0xb82ce77fe7ba7253f133b7b6935df0545e33dbb9',
